11a4faae852bedb9ccce187d508946bf9467f673
plumber <sha.a.wf@gmail.com>
2021-09-10 18:29:55 +0430

    many changes.

--- code.ts
+++ code.ts
@@ -5,7 +5,22 @@ figma.ui.resize(500,500)
 
 
 
-var selected_text_nodes = [];
+
+
+class TextNodeData{
+  id: string;
+  node: TextNode;
+  text : string;
+  final_text :string;
+
+
+  constructor(node: TextNode){
+    this.id =node.id;
+    this.node= node;
+    this.text = node.characters;
+    this.final_text = "";
+  }
+} 
 
 const LTR  = 'LTR';
 const RTL = 'RTL'
@@ -19,44 +34,45 @@ function changeDirection(){
   }
 }
 
-function detectTexts(frame:FrameNode){
+ function detectTexts(frame:FrameNode){
       
   const textNodes:TextNode[] = frame.children.filter(node =>node.type ==="TEXT") as TextNode[]
 
-  textNodes.forEach(async text_node => {
-        await figma.loadFontAsync(<FontName> text_node.fontName);
+  textNodes.forEach(text_node => {
+        // await figma.loadFontAsync(<FontName> text_node.fontName);
         
-        if(selected_text_nodes.find(element=>element.node ===text_node) == null){
-          selected_text_nodes.push({
-            "id" : text_node.id,
-            "node": text_node,
-            "text" : text_node.characters,
-            "final_text" : ""
-          });
+        if(selected_text_nodes.find(element=>element.node.id ===text_node.id) == null){ 
+          selected_text_nodes.push(new TextNodeData(text_node));
         }
-
-        text_node.characters = "aa" + selected_text_nodes.length
 
   });
 }
 
 
+function reverseString(str:string) {
+  return str.split("").reverse().join("");
+}
+
+var selected_text_nodes:Array<TextNodeData> = [];
+
+figma.on("selectionchange", () => {
+  selected_text_nodes = [];
+  const frame:FrameNode = figma.currentPage.selection.find(node => node.type ==="FRAME") as FrameNode;
+  detectTexts(frame);
+
+  figma.ui.postMessage({
+    'type' : "detect_texts",
+    'data' : selected_text_nodes
+  })
+
+  console.warn(selected_text_nodes)
+})
 
 figma.ui.onmessage = async msg => {
 
-  var command_type = msg['type'];
-  switch(command_type){
-    case "detect_texts" :
-     
-      const frame:FrameNode = figma.currentPage.selection.find(node => node.type ==="FRAME") as FrameNode;
-      detectTexts(frame);
+  var command_type = msg['type']; 
 
-      figma.ui.postMessage({
-        'type' : command_type,
-        'data' : selected_text_nodes
-      })
-      
-    return;
+  switch(command_type){
     case "change_direction" :
    
       changeDirection();
@@ -68,7 +84,18 @@ figma.ui.onmessage = async msg => {
       
     return;
     case "apply_changes":
-
+      const final_data:Array<TextNodeData> = msg['text_data'] as Array<TextNodeData>;
+      selected_text_nodes.forEach(async node_data =>{
+        var text_node = node_data.node;
+        var selected_text = final_data.find(d => d.id == text_node.id);
+        if(selected_text.final_text.length !== 0){
+          if(direction === LTR){
+            text_node.characters = selected_text.final_text;
+          }else{
+            text_node.characters = reverseString(selected_text.final_text);
+          }
+        } 
+      })
     break;
   }
   figma.closePlugin();
