abee3e89461b46e89fafc83040173a4e6f311a13
plumber <sha.a.wf@gmail.com>
2021-09-10 17:16:49 +0430

    code updated

--- code.ts
+++ code.ts
@@ -1,33 +1,72 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
 
-// This shows the HTML page in "ui.html".
+var selected_text_nodes = [];
+
+var direction = 'fa';
+
+function changeDirection(){
+  if(direction === 'fa'){
+    direction = 'en'
+  }else{
+    direction = 'fa'
+  }
+}
+
+function detectTexts(frame:FrameNode){
+      
+  const textNodes:TextNode[] = frame.children.filter(node =>node.type ==="TEXT") as TextNode[]
+
+  textNodes.forEach(async text_node => {
+        await figma.loadFontAsync(<FontName> text_node.fontName);
+        
+        if(selected_text_nodes.find(element=>element.node ===text_node) == null){
+          selected_text_nodes.push({
+            "id" : text_node.id,
+            "node": text_node,
+            "text" : text_node.characters,
+            "final_text" : ""
+          });
+        }
+
+        text_node.characters = "aa" + selected_text_nodes.length
+
+  });
+}
+
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
+figma.ui.onmessage = async msg => {
+
+  var command_type = msg['type'];
+  switch(command_type){
+    case "detect_texts" :
+     
+      const frame:FrameNode = figma.currentPage.selection.find(node => node.type ==="FRAME") as FrameNode;
+      detectTexts(frame);
+
+      figma.ui.postMessage({
+        'type' : 'detected_texts',
+        'data' : selected_text_nodes
+      })
+      
+    return;
+    case "change_direction" :
+   
+      changeDirection();
+
+      figma.ui.postMessage( {
+        'type' : 'direction',
+        'data' : direction
+      })
+      
+    return;
+    case "apply_changes":
+      
+    break;
+  }
   figma.closePlugin();
+
 };
+
+
