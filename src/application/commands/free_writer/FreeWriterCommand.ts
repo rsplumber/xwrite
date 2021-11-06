9b378e3023e88c3514546fb2549d364f52e9aa81
plumber <sha.a.wf@gmail.com>
2021-11-06 04:08:49 +0330

    FreeWriter added.

--- src/application/commands/free_writer/FreeWriterCommand.ts
+++ src/application/commands/free_writer/FreeWriterCommand.ts
@@ -8,16 +8,15 @@ export class FreeWriterCommand extends AbstractCommand {
 
 
     execute(request: Request): Response {
-
-        const finalText = request.data['text'];
+        const finalText = request.getValue("data") as string;
         const directionFixedText = TextDirectionFixer.fix(finalText);
-
         Context.getTextNodesContainer().getAll().forEach(async nodeData => {
             const textNode = nodeData.node as TextNode;
             await figma.loadFontAsync(textNode.fontName as FontName);
             textNode.characters = directionFixedText;
         });
-        return undefined;
+        return Context.responseGenerator(true)
+            .generate();
     }
 
     identifier(): string {
